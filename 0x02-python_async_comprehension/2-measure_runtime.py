d467d47e6c635b2a2d2bd3f0e8cb0c651669d8a5
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-21 11:18:03 +0800

    Pushing Exo 2 Measure Runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -2,3 +2,14 @@
 """
     Here the module description for exo 2
 """
+import asyncio
+import time
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ Measuring Runtime """
+    start = time.perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    end = time.perf_counter()
+    return end - start
