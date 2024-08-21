045eea1cd6c264ac415d70b1843db5a829d10f9f
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-21 12:06:30 +0800

    Corrections

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Any, AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, Any]:
     """ Async Geneator co routine """
     for _ in range(10):
         await asyncio.sleep(1)
