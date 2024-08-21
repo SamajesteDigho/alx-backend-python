096dff358d8c245f611c5339de4128845129ce5e
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-21 11:11:52 +0800

    Commiting Exo 1 Async Comprehensive

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,3 +2,12 @@
 """
     Here the module description for exo 1
 """
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """ Async Comprehension coroutine """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
