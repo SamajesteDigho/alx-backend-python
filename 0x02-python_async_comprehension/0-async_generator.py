fabdbaa4230e019e1ea143dc631ee712f470168f
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-23 09:34:32 +0800

    Exo 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Any, AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, Any]:
+async def async_generator() -> AsyncGenerator[float, float]:
     """ Async Geneator co routine """
     for _ in range(10):
         await asyncio.sleep(1)
