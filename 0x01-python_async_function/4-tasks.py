c904fe2c03847bedb6a80d87b3a47f5691efb533
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-23 09:48:17 +0800

    Corretion Exo 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,4 +9,5 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Waiting async routine """
-    return [await task_wait_random(max_delay) for _ in range(n)]
+    data = [await task_wait_random(max_delay) for _ in range(n)]
+    return sorted(data)
--- 0x01-python_async_function/main.py
+++ 0x01-python_async_function/main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-'''
-Test file for printing the correct output of the wait_n coroutine
-'''
+
 import asyncio
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+task_wait_n = __import__('4-tasks').task_wait_n
 
-print(asyncio.run(wait_n(5, 5)))
-print(asyncio.run(wait_n(10, 7)))
-print(asyncio.run(wait_n(10, 0)))
+n = 5
+max_delay = 6
+print(asyncio.run(task_wait_n(n, max_delay)))
